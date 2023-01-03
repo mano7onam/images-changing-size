2baff3d6d4cc8771a4555d4fdb7b9e24c62a74bc
andrey.matveev <amatveev@jetbrains.com>
2023-01-03 21:24:35 +0200

    Cnahge after upscale

--- after_upscale.py
+++ after_upscale.py
@@ -7,13 +7,13 @@ DIR_WITH_IMAGES = '/Users/andrey.matveev/aspire/MIDJORNEY_IMAGES'
 DIR_WITH_IMAGES = os.path.join(os.getcwd(), "output")
 OUTPUT_DIR = os.path.join(os.getcwd(), "output_new")
 INPUT_FORMAT = '.png'
-OUTPUT_FORMAT = '.png'
+OUTPUT_FORMAT = '.jpg'
 
 SIZES = ((8, 10), (11, 14), (16, 20), (83, 117))
 
 def process_image(image_path):
     img = Image.open(image_path)
-    img_name = os.path.basename(image_path).split('.png')[0]
+    img_name = os.path.basename(image_path).split(INPUT_FORMAT)[0]
 
     for size in SIZES:
         w = img.width
@@ -25,7 +25,7 @@ def process_image(image_path):
         dpi = w // ww
         if ww == 83:
             dpi *= 10
-        res_name = f'{w}_{h}_{dpi}_{img_name}.jpg'
+        res_name = f'{w}_{h}_{dpi}_{img_name}{OUTPUT_FORMAT}'
         res_dir_name = f'{ww}x{hh}'
         res_dir_path = os.path.join(OUTPUT_DIR, res_dir_name)
         if not os.path.isdir(res_dir_path):
@@ -33,5 +33,6 @@ def process_image(image_path):
         img.save(os.path.join(res_dir_path, res_name), dpi=(dpi, dpi))
 
 
-for image_path in glob.glob(f'{DIR_WITH_IMAGES}/*.png'):
+for image_path in glob.glob(f'{DIR_WITH_IMAGES}/*{INPUT_FORMAT}'):
+    print(image_path)
     process_image(image_path)
