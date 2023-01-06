c5b4cc111da5e38513dde5bec8bfdc9fccb28812
andrey.matveev <amatveev@jetbrains.com>
2023-01-06 13:34:14 +0200

    Impl img resizer

--- resize_images.py
+++ resize_images.py
@@ -4,18 +4,21 @@ import glob
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = 933120000
 
+DESIRED_SIZE_W = 512
+DESIRED_SIZE_H = 512
+
 DIR_WITH_IMAGES = '/Users/andrey.matveev/aspire/MIDJORNEY_IMAGES'
 OUTPUT_DIR = os.path.join(os.getcwd(), "output")
 
 INPUT_FORMAT = '.png'
-OUTPUT_FORMAT = '.png'
+OUTPUT_FORMAT = '.jpg'
 
 
 def process_image(image_path):
     img = Image.open(image_path)
-    res_image = img.resize((1000, 1000))
+    res_image = img.resize((DESIRED_SIZE_W, DESIRED_SIZE_H))
     img_name = os.path.basename(image_path).split(INPUT_FORMAT)[0]
-    res_name = f'1000x1000_{img_name}{OUTPUT_FORMAT}'
+    res_name = f'{DESIRED_SIZE_W}x{DESIRED_SIZE_H}_{img_name}{OUTPUT_FORMAT}'
     res_path = os.path.join(OUTPUT_DIR, res_name)
     res_image.save(res_path)
 
