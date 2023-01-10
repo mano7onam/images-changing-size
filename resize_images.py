e20511f83eeb0a308d9ccfb2dbb132386a906364
andrey.matveev <amatveev@jetbrains.com>
2023-01-10 11:56:26 +0200

    QQQ

--- resize_images.py
+++ resize_images.py
@@ -4,26 +4,30 @@ import glob
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = 933120000
 
-DESIRED_SIZE_W = 512
-DESIRED_SIZE_H = 512
+DESIRED_SIZE_W = 1024
+DESIRED_SIZE_H = 1024
 
 DIR_WITH_IMAGES = '/Users/andrey.matveev/aspire/IMGS/WATERCOLOUR'
-OUTPUT_DIR = '/Users/andrey.matveev/aspire/IMGS/WATERCOLOUR_RESIZED'
+OUTPUT_DIR = '/Users/andrey.matveev/aspire/IMGS/WATERCOLOUR_RESIZED_1024'
 if not os.path.isdir(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
-INPUT_FORMAT = '.png'
-OUTPUT_FORMAT = '.jpg'
+INPUT_FORMAT = ['.png', '.jpg']
+OUTPUT_FORMAT = '.jpeg'
 
 
-def process_image(image_path):
+def process_image(image_path, input_format):
     img = Image.open(image_path)
     res_image = img.resize((DESIRED_SIZE_W, DESIRED_SIZE_H))
-    img_name = os.path.basename(image_path).split(INPUT_FORMAT)[0]
+    img_name = os.path.basename(image_path).split(input_format)[0]
     res_name = f'{DESIRED_SIZE_W}x{DESIRED_SIZE_H}_{img_name}{OUTPUT_FORMAT}'
     res_path = os.path.join(OUTPUT_DIR, res_name)
     res_image.save(res_path)
 
 
-for image_path in glob.glob(f'{DIR_WITH_IMAGES}/*{INPUT_FORMAT}'):
-    process_image(image_path)
+cnt = 0
+for input_format in INPUT_FORMAT:
+    for image_path in glob.glob(f'{DIR_WITH_IMAGES}/*{input_format}'):
+        cnt += 1
+        process_image(image_path, input_format)
+print(cnt)
