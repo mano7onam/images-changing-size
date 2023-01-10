f4d2a76d47d7a5d4741066f03f92919ffe35c079
andrey.matveev <amatveev@jetbrains.com>
2023-01-10 17:28:42 +0200

    Impl crop and remove background

--- crop_image.py
+++ crop_image.py
@@ -1,44 +1,66 @@
+import glob
+import os.path
+import sys
+
 from PIL import Image
+from tqdm import tqdm
 
-input_path = '/Users/andrey.matveev/Downloads/IMG_0572.png'
-img = Image.open(input_path)
-WHITE = (255, 255, 255)
 
-imin = 1e10
-imax = -1e10
-jmin = 1e10
-jmax = -1e10
-for i in range(0, img.width):
-    for j in range(0, img.height):
-        col = img.getpixel((i, j))
-        if len(col) == 4:
-            if col == (0, 0, 0, 0):
-                continue
-            imin = min(imin, i)
-            imax = max(imax, i)
-            jmin = min(jmin, j)
-            jmax = max(jmax, j)
-        else:
-            print('aaaa')
-imin = max(0, imin - 10)
-imax = min(img.width, imax + 10)
-jmin = max(0, jmin - 10)
-jmax = min(img.height, jmax + 10)
+def crop_image(input_path, output_path):
+    img = Image.open(input_path)
+    imin = 1e10
+    imax = -1e10
+    jmin = 1e10
+    jmax = -1e10
+    flag = False
+    for i in range(0, img.width):
+        for j in range(0, img.height):
+            col = img.getpixel((i, j))
+            if len(col) == 4:
+                if col == (0, 0, 0, 0):
+                    continue
+                imin = min(imin, i)
+                imax = max(imax, i)
+                jmin = min(jmin, j)
+                jmax = max(jmax, j)
+                flag = True
+            else:
+                print('aaaa')
+    if not flag:
+        return
+    imin = max(0, imin - 10)
+    imax = min(img.width, imax + 10)
+    jmin = max(0, jmin - 10)
+    jmax = min(img.height, jmax + 10)
+    print(input_path, file=sys.stderr)
+    print(imin, imax, jmin, jmax, file=sys.stderr)
 
-crop_img = img.crop((imin, jmin, imax, jmax))
-fill_col = (255, 255, 255, 255)
-for i in range(0, crop_img.width):
-    for j in range(0, crop_img.height):
-        col = crop_img.getpixel((i, j))
-        if len(col) == 4 and col != (0, 0, 0, 0):
-            fill_col = col
-            break
-print(fill_col)
+    crop_img = img.crop((imin, jmin, imax, jmax))
+    fill_col = (255, 255, 255, 255)
+    for i in range(0, crop_img.width):
+        for j in range(0, crop_img.height):
+            col = crop_img.getpixel((i, j))
+            if len(col) == 4 and col != (0, 0, 0, 0):
+                fill_col = col
+                break
+    print(fill_col, file=sys.stderr)
 
-for i in range(0, crop_img.width):
-    for j in range(0, crop_img.height):
-        col = crop_img.getpixel((i, j))
-        if len(col) == 4 and col == (0, 0, 0, 0):
-            crop_img.putpixel((i, j), fill_col)
+    for i in range(0, crop_img.width):
+        for j in range(0, crop_img.height):
+            col = crop_img.getpixel((i, j))
+            if len(col) == 4 and col == (0, 0, 0, 0):
+                crop_img.putpixel((i, j), fill_col)
 
-crop_img.save('crop.png')
+    crop_img.save(output_path)
+
+
+out_dir = 'output_dir'
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
+
+imgs_paths = glob.glob('/Users/andrey.matveev/aspire/IMGS/TO_RM_BG/*.png')
+for img_path in tqdm(imgs_paths):
+    basename = os.path.basename(img_path)
+    output_path = os.path.join(out_dir, basename)
+    crop_image(img_path, output_path)
+
--- remove_bg_images.py
+++ remove_bg_images.py
@@ -1,10 +1,23 @@
-from rembg import remove
+import glob
+import os.path
+
 from PIL import Image
+from rembg import remove
+from tqdm import tqdm
 
-input_path = 'crop.png'
-# input_path = 'output.png'
-output_path = 'output42.png'
+out_dir = 'output_dir_rm_bg'
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
 
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
+
+def remove_bg(img_path, output_path):
+    input = Image.open(img_path)
+    output = remove(input)
+    output.save(output_path)
+
+
+imgs_paths = glob.glob('output_dir/*.png')
+for img_path in tqdm(imgs_paths):
+    basename = os.path.basename(img_path)
+    output_path = os.path.join(out_dir, basename)
+    remove_bg(img_path, output_path)
