2b3b985e328b5f15bd2cabb06fc6a1ce9bf90924
andrey.matveev <amatveev@jetbrains.com>
2023-01-11 18:22:09 +0200

    Change resize images

--- resize_images.py
+++ resize_images.py
@@ -4,11 +4,11 @@ import glob
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = 933120000
 
-DESIRED_SIZE_W = 1024
-DESIRED_SIZE_H = 1024
+DESIRED_SIZE_W = 512
+DESIRED_SIZE_H = 512
 
 DIR_WITH_IMAGES = '/Users/andrey.matveev/aspire/IMGS/WATERCOLOUR'
-OUTPUT_DIR = '/Users/andrey.matveev/aspire/IMGS/WATERCOLOUR_RESIZED_1024'
+OUTPUT_DIR = '/Users/andrey.matveev/aspire/IMGS/WATERCOLOUR_RESIZED_512_JPEG'
 if not os.path.isdir(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
