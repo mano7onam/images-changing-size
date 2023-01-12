2a92b5ebda54e94ffc014862684456f7b9bd2166
andrey.matveev <amatveev@jetbrains.com>
2023-01-12 20:09:38 +0200

    Start good impl using api

--- use_api_stablediffusion.py
+++ use_api_stablediffusion.py
@@ -1,33 +1,78 @@
+import glob
 import json
 import requests
 import io
 import base64
 from PIL import Image, PngImagePlugin
 
-url = "http://127.0.0.1:7860"
+URL = "http://127.0.0.1:7860"
 
-payload = {
-    "prompt": "",
-    "negative_prompt": "",
-    "steps": 20,
-    "seed": -1,
-    "width": 512,
-    "height": 768,
-    "cfg_scale": 7,
-}
 
-response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+def process_files_get_prompts(files_or_globs):
+    all_files = []
+    for file in files_or_globs:
+        if file.startswith('glob::::'):
+            glob_str = file.split('::::')[1]
+            cur_files = glob.glob(glob_str)
+            all_files.extend(cur_files)
 
-r = response.json()
+    res_all_prompts = []
+    for file in all_files:
+        with open(file, 'r') as f:
+            for line in f:
+                res_all_prompts.append(line)
+    return res_all_prompts
 
-for i in r['images']:
-    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
 
-    png_payload = {
-        "image": "data:image/png;base64," + i
+def process_request(prompt, negative_prompt):
+    payload = {
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "steps": 20,
+        "seed": -1,
+        "width": 512,
+        "height": 768,
+        "cfg_scale": 7,
     }
-    response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
 
-    pnginfo = PngImagePlugin.PngInfo()
-    pnginfo.add_text("parameters", response2.json().get("info"))
-    image.save('generated.png', pnginfo=pnginfo)
+    response = requests.post(url=f'{URL}/sdapi/v1/txt2img', json=payload)
+
+    r = response.json()
+
+    for i in r['images']:
+        image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
+
+        png_payload = {
+            "image": "data:image/png;base64," + i
+        }
+        response2 = requests.post(url=f'{URL}/sdapi/v1/png-info', json=png_payload)
+
+        pnginfo = PngImagePlugin.PngInfo()
+        pnginfo.add_text("parameters", response2.json().get("info"))
+        image.save('generated.png', pnginfo=pnginfo)
+
+
+def start():
+    with open("config.json") as f:
+        obj = json.load(f)
+        all_models = obj['models']
+        all_prompts = process_files_get_prompts(obj['files_with_prompts'])
+        all_negative_prompts = process_files_get_prompts(obj['negative_prompts'])
+        print(all_models)
+        print(all_prompts)
+        print(all_negative_prompts)
+
+    for model in all_models:
+        option_payload = {
+            "sd_model_checkpoint": model
+        }
+        response = requests.post(url=f'{URL}/sdapi/v1/options', json=option_payload)
+        print(response.status_code)
+        if response.status_code != 200:
+            continue
+        for prompt in all_prompts:
+            for negative_prompt in all_negative_prompts:
+                process_request(prompt, negative_prompt)
+
+
+start()
